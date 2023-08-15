b911e5a222340b0dd0381797805fd1470cb6fb56
tejasbr4004 <106443279+tejasbr4004@users.noreply.github.com>
2023-08-15 23:17:06 +0530

    e

--- src/components/Songlists.js
+++ src/components/Songlists.js
@@ -6,7 +6,7 @@ export const Songlists=[
         song_name:"akasha este yakedyu",
         singer:"",
         image_src:require("../assets/songs_images/akasha_este_yakedyu.jpeg") ,
-        song_src: require("../assets/audio_tracks/Shape_of_You.mp3"),
+        song_src: require("../assets/audio_tracks/akasha_este_yakedyu.mp3"),
     },
     {
         id:"32",
